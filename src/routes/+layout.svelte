e88b74d41a9c4e17b9647e2609580db73a1fc5f3
Lou Guberti Ng <louguberti@gmail.com>
2025-12-16 23:03:46 +0800

    Add Momo to the footer.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,22 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div class="navbar bg-base-300 sticky top-0">
-  <button class="btn btn-ghost text-xl">Aspect of Momus</button>
+<div class="navbar sticky top-0 bg-base-300">
+	<button class="btn text-xl btn-ghost">Aspect of Momus</button>
 </div>
 {@render children()}
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-  <aside>
-    <p>Copyright © {new Date().getFullYear()+12} - All rights reserved by Momus</p>
-	<p>Momus does not now nor has it ever promoted online falsehoods</p>
-	<p>Random Disclaimer: This blog is a work of speculative fiction and if you believe it that is on you, babe.</p>
-  </aside>
+<footer class="footer-center footer bg-base-300 p-4 text-base-content sm:footer-horizontal">
+	<div class="avatar">
+		<div class="mask w-24 mask-circle">
+			<img class="object-top grayscale" src="src/lib/assets/Momus_Tarot.jpg" />
+		</div>
+	</div>
+	<aside>
+		<p>Copyright © {new Date().getFullYear() + 12} - All rights reserved by Momus</p>
+		<p>Momus does not now nor has it ever promoted online falsehoods</p>
+		<p>
+			Random Disclaimer: This blog is a work of speculative fiction and if you believe it that is on
+			you, babe.
+		</p>
+	</aside>
 </footer>
